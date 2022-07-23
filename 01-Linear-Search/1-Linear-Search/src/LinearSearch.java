7573b6ef331a96e3d00765d0cec006e4848278f9
Jiajia Liang <jiajialiang.tlo@gmail.com>
2022-07-23 10:23:53 +0800

    Linear-Search

---
+++ 01-Linear-Search/1-Linear-Search/src/LinearSearch.java
@@ -0,0 +1,11 @@
+public class LinearSearch {
+    private LinearSearch() {
+    }
+
+    public static int search(int[] data, int target) {
+
+        for (int i = 0; i < data.length; i++)
+            if (data[i] == target) return i;
+        return -1;
+    }
+}
