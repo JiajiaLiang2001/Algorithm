b1b5f33bdafd6c47434bf7a5c356f49f215f1dbd
Jiajia Liang <jiajialiang.tlo@gmail.com>
2022-07-27 16:14:44 +0800

    DummyHead-in-LinkedList

---
+++ 06-Linked-List/06-3-DummyHead-in-LinkedList/src/LinkedList.java
@@ -0,0 +1,74 @@
+public class LinkedList<E> {
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private Node dummyHead;
+    private int size;
+
+    public LinkedList() {
+        dummyHead = new Node();
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * Add node to linked list
+     *
+     * @param index
+     * @param e
+     */
+    public void add(int index, E e) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Add failed. Illegal index.");
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++)
+            prev = prev.next;
+        prev.next = new Node(e, prev.next);
+        size++;
+    }
+
+    /**
+     * Add a node to the head of the linked list
+     *
+     * @param e
+     */
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    /**
+     * Add a node to the end of the linked list
+     *
+     * @param e
+     */
+    public void addLast(E e) {
+        add(size, e);
+    }
+}
