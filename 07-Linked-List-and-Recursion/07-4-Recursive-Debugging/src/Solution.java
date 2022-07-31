8a356ec1ab4f56dbbc89a589dcf08e3bb52f7f07
Jiajia Liang <jiajialiang.tlo@gmail.com>
2022-07-31 10:24:20 +0800

    Recursive-Debugging

---
+++ 07-Linked-List-and-Recursion/07-4-Recursive-Debugging/src/Solution.java
@@ -0,0 +1,37 @@
+public class Solution {
+    private String generateDepthString(int depth) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < depth; i++)
+            res.append("--");
+        return res.toString();
+    }
+
+    public ListNode removeElements(ListNode head, int val, int depth) {
+        String depthString = generateDepthString(depth);
+
+        System.out.print(depthString);
+        System.out.println("Call: remove " + val + " in " + head);
+
+        if (head == null) {
+            System.out.print(depthString);
+            System.out.println("Return: " + head);
+            return head;
+        }
+
+        ListNode res = removeElements(head.next, val, depth + 1);
+        System.out.print(depthString);
+        System.out.println("After remove " + val + ": " + res);
+
+        ListNode ret;
+        if (head.val == val)
+            ret = res;
+        else {
+            head.next = res;
+            ret = head;
+        }
+        System.out.print(depthString);
+        System.out.println("Return: " + ret);
+
+        return ret;
+    }
+}
