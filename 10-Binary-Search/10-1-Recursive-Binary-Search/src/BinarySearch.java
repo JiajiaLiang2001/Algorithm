4a778494f1f1f94a4088f2e45939e39a045e1607
Jiajia Liang <jiajialiang.tlo@gmail.com>
2022-08-11 21:51:59 +0800

    Recursive-Binary-Search

---
+++ 10-Binary-Search/10-1-Recursive-Binary-Search/src/BinarySearch.java
@@ -0,0 +1,18 @@
+public class BinarySearch {
+    private BinarySearch() {
+    }
+
+    public static <E extends Comparable<E>> int search(E[] data, E target) {
+        return search(data, 0, data.length - 1, target);
+    }
+
+    private static <E extends Comparable<E>> int search(E[] data, int l, int r, E target) {
+        if (l > r) return -1;
+        int mid = l + (r - l) / 2;
+        if (data[mid].compareTo(target) == 0)
+            return mid;
+        if (data[mid].compareTo(target) < 0)
+            return search(data, mid + 1, r, target);
+        return search(data, l, mid - 1, target);
+    }
+}
