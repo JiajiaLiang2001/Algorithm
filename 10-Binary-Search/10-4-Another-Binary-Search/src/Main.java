e80c5971d5d9bfc2f0a4e6b744416cca40fb15cd
Jiajia Liang <jiajialiang.tlo@gmail.com>
2022-08-17 23:02:50 +0800

    Another-Binary-Search

---
+++ 10-Binary-Search/10-4-Another-Binary-Search/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Integer[] nums = {2, 4, 7, 9, 11, 24, 56};
+        int index = BinarySearch.search(nums, 11);
+        System.out.println(index);
+    }
+}
