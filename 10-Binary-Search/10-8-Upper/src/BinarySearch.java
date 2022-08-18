0d461127ba6355c2badb44f7c296aa92bec32caa
Jiajia Liang <jiajialiang.tlo@gmail.com>
2022-08-18 20:49:49 +0800

    Upper

---
+++ 10-Binary-Search/10-8-Upper/src/BinarySearch.java
@@ -0,0 +1,44 @@
+public class BinarySearch {
+    private BinarySearch() {
+    }
+
+    public static <E extends Comparable<E>> int search(E[] data, E target) {
+        int l = 0, r = data.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (data[mid].compareTo(target) == 0)
+                return mid;
+            if (data[mid].compareTo(target) < 0)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return -1;
+    }
+
+    public static <E extends Comparable<E>> int searchR(E[] data, E target) {
+        return searchR(data, 0, data.length - 1, target);
+    }
+
+    private static <E extends Comparable<E>> int searchR(E[] data, int l, int r, E target) {
+        if (l > r) return -1;
+        int mid = l + (r - l) / 2;
+        if (data[mid].compareTo(target) == 0)
+            return mid;
+        if (data[mid].compareTo(target) < 0)
+            return searchR(data, mid + 1, r, target);
+        return searchR(data, l, mid - 1, target);
+    }
+
+    public static <E extends Comparable<E>> int upper(E[] data, E target) {
+        int l = 0, r = data.length;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (data[mid].compareTo(target) <= 0)
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        return l;
+    }
+}
