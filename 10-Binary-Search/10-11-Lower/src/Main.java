b4ff7b6d1b950aa5716a054b26b14070b8651a98
Jiajia Liang <jiajialiang.tlo@gmail.com>
2022-08-18 22:04:02 +0800

    Lower

---
+++ 10-Binary-Search/10-11-Lower/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Integer[] nums = {23, 56, 65, 69, 72, 89, 96, 99};
+        int index = BinarySearch.lower(nums, 60);// 2
+        System.out.println(index);
+    }
+}
