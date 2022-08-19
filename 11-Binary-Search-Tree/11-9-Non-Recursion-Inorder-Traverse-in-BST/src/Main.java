583129ada236753dd3093305ab9d823de62cbfe0
Jiajia Liang <jiajialiang.tlo@gmail.com>
2022-08-20 01:14:13 +0800

    Modify-The-Use-Case

--- 11-Binary-Search-Tree/11-9-Non-Recursion-Inorder-Traverse-in-BST/src/Main.java
+++ 11-Binary-Search-Tree/11-9-Non-Recursion-Inorder-Traverse-in-BST/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BST<Integer> bst = new BST<>();
-        int[] nums = {28,16,30,13,22,29,42};
+        int[] nums = {5, 3, 6, 8, 4, 2};
         for (int num : nums)
             bst.add(num);
         bst.inOrder();
