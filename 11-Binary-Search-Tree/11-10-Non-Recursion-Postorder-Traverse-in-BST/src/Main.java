26bf1d942855a53000e191cd0371aa70f719a290
Jiajia Liang <jiajialiang.tlo@gmail.com>
2022-08-20 00:58:50 +0800

    Non-Recursion-Postorder-Traverse-in-BST

---
+++ 11-Binary-Search-Tree/11-10-Non-Recursion-Postorder-Traverse-in-BST/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+        int[] nums = {28, 16, 30, 13, 22, 29, 42};
+        for (int num : nums)
+            bst.add(num);
+        bst.postOrder();
+        System.out.println();
+        bst.postOrderNR();
+        System.out.println();
+        /////////////////
+        //      5      //
+        //    /   \    //
+        //   3    6    //
+        //  / \    \   //
+        // 2  4     8  //
+        /////////////////
+        System.out.println(bst);
+    }
+}
