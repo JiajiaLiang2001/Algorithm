bc1df9e061b37d3c2030357a79523459866b4e6c
Jiajia Liang <jiajialiang.tlo@gmail.com>
2022-08-20 12:43:35 +0800

    Remove-Min-and-Max-in-BST

---
+++ 11-Binary-Search-Tree/11-12-Remove-Min-and-Max-in-BST/src/Main.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+        int[] nums = {5, 3, 6, 8, 4, 2};
+
+        /////////////////
+        //      5      //
+        //    /   \    //
+        //   3    6    //
+        //  / \    \   //
+        // 2  4     8  //
+        /////////////////
+        System.out.println(bst);
+
+        for (int num : nums)
+            bst.add(num);
+        testRemoveMin(bst);
+
+        for (int num : nums)
+            bst.add(num);
+        testRemoveMax(bst);
+    }
+
+    public static <E extends Comparable<E>> void testRemoveMin(BST<E> bst) {
+        ArrayList<E> nums = new ArrayList<>();
+        while (!bst.isEmpty())
+            nums.add(bst.removeMin());
+        isSmallToLarge(nums);
+        System.out.println("removeMin test completed.");
+        traversePrint(nums);
+    }
+
+    public static <E extends Comparable<E>> void isSmallToLarge(ArrayList<E> nums) {
+        for (int i = 1; i < nums.size(); i++)
+            if (nums.get(i - 1).compareTo(nums.get(i)) > 0)
+                throw new IllegalArgumentException("Error!");
+    }
+
+    public static <E extends Comparable<E>> void testRemoveMax(BST<E> bst) {
+        ArrayList<E> nums = new ArrayList<>();
+        while (!bst.isEmpty())
+            nums.add(bst.removeMax());
+        isLargeToSmall(nums);
+        System.out.println("removeMax test completed.");
+        traversePrint(nums);
+    }
+
+    public static <E extends Comparable<E>> void isLargeToSmall(ArrayList<E> nums) {
+        for (int i = 1; i < nums.size(); i++)
+            if (nums.get(i - 1).compareTo(nums.get(i)) < 0)
+                throw new IllegalArgumentException("Error!");
+    }
+
+    public static <E extends Comparable<E>> void traversePrint(ArrayList<E> nums) {
+        for (E num : nums) System.out.println(num);
+    }
+}
