a96791b01243e35512d45f3ad84eb36020739e99
Jiajia Liang <jiajialiang.tlo@gmail.com>
2022-08-21 16:10:24 +0800

    Fix-Remove

--- 11-Binary-Search-Tree/11-13-Remove-Elements/src/Main.java
+++ 11-Binary-Search-Tree/11-13-Remove-Elements/src/Main.java
@@ -1,55 +1,13 @@
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
         BST<Integer> bst = new BST<>();
-        int[] nums = {28, 16, 30, 13, 29, 42};
-
-        /////////////////
-        //      5      //
-        //    /   \    //
-        //   3    6    //
-        //  / \    \   //
-        // 2  4     8  //
-        /////////////////
+        int[] nums = {41, 16, 58, 13, 50, 60, 42, 53, 59, 63};
         for (int num : nums) {
             bst.add(num);
         }
-        bst.remove(30);
-    }
-
-    public static <E extends Comparable<E>> void testRemoveMin(BST<E> bst) {
-        ArrayList<E> nums = new ArrayList<>();
-        while (!bst.isEmpty())
-            nums.add(bst.removeMin());
-        isSmallToLarge(nums);
-        System.out.println("removeMin test completed.");
-        traversePrint(nums);
-    }
-
-    public static <E extends Comparable<E>> void isSmallToLarge(ArrayList<E> nums) {
-        for (int i = 1; i < nums.size(); i++)
-            if (nums.get(i - 1).compareTo(nums.get(i)) > 0)
-                throw new IllegalArgumentException("Error!");
-    }
-
-    public static <E extends Comparable<E>> void testRemoveMax(BST<E> bst) {
-        ArrayList<E> nums = new ArrayList<>();
-        while (!bst.isEmpty())
-            nums.add(bst.removeMax());
-        isLargeToSmall(nums);
-        System.out.println("removeMax test completed.");
-        traversePrint(nums);
-    }
-
-    public static <E extends Comparable<E>> void isLargeToSmall(ArrayList<E> nums) {
-        for (int i = 1; i < nums.size(); i++)
-            if (nums.get(i - 1).compareTo(nums.get(i)) < 0)
-                throw new IllegalArgumentException("Error!");
-    }
-
-    public static <E extends Comparable<E>> void traversePrint(ArrayList<E> nums) {
-        for (E num : nums) System.out.print(num + " ");
+        bst.remove(58);
+        bst.inOrderNR();
         System.out.println();
+        System.out.println(bst);
     }
 }
