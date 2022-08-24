599f8dcaa438bc49b7dc4e3d7a3a1ee21b948c1b
Jiajia Liang <jiajialiang.tlo@gmail.com>
2022-08-24 23:52:28 +0800

    Unique-Morse-Code-Words

---
+++ 11-Binary-Search-Tree/11-16-Set/11-16-4-Unique-Morse-Code-Words/src/Solution.java
@@ -0,0 +1,17 @@
+/// Leetcode 804. Unique Morse Code Words
+/// https://leetcode.cn/problems/unique-morse-code-words/
+import java.util.TreeSet;
+
+public class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] codes = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        TreeSet<String> set = new TreeSet<>();
+        for (String word : words) {
+            StringBuilder res = new StringBuilder();
+            for (int i = 0; i < word.length(); i++)
+                res.append(codes[word.charAt(i) - 'a']);
+            set.add(res.toString());
+        }
+        return set.size();
+    }
+}
