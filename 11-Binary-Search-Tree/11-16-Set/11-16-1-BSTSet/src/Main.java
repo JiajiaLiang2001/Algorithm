9026ce4c420126ff03233e1566cb7c2f7b232767
Jiajia Liang <jiajialiang.tlo@gmail.com>
2022-08-24 09:45:04 +0800

    Set-BSTSet

---
+++ 11-Binary-Search-Tree/11-16-Set/11-16-1-BSTSet/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Pride and Prejudice");
+        ArrayList<String> words1 = new ArrayList<>();
+        BSTSet<String> set1 = new BSTSet<>();
+        if (FileOperation.readFile("11-Binary-Search-Tree/11-16-Set/11-16-1-BSTSet/src/text/pride-and-prejudice.txt", words1)) {
+            System.out.println("Total words: " + words1.size());
+
+            for (String word : words1)
+                set1.add(word);
+            System.out.println("Total different words: " + set1.getSize());
+        }
+        System.out.println();
+        System.out.println("A Tale of Two Cities");
+        ArrayList<String> words2 = new ArrayList<>();
+        BSTSet<String> set2 = new BSTSet<>();
+        if (FileOperation.readFile("11-Binary-Search-Tree/11-16-Set/11-16-1-BSTSet/src/text/a-tale-of-two-cities.txt", words2)) {
+            System.out.println("Total words: " + words2.size());
+            for (String word : words2)
+                set2.add(word);
+            System.out.println("Total different words: " + set2.getSize());
+        }
+    }
+}
